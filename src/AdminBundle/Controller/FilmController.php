51d025334ac89b08d04b62d14e40dc157848c604
Samuel Besnier <sbesnier1901@gmail.com>
2017-07-05 14:35:07 +0200

    Fonctions ajout et modification de film fonctionnelles

--- src/AdminBundle/Controller/FilmController.php
+++ src/AdminBundle/Controller/FilmController.php
@@ -2,8 +2,12 @@
 
 namespace AdminBundle\Controller;
 
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
+use ModelBundle\Entity\Movie;
+use ModelBundle\Form\MovieType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Class DefaultController
@@ -30,9 +34,52 @@ class FilmController extends Controller
      *
      * @Route("/formulaire/{id}", name="admin_film_edit")
      * @Route("/formulaire", name="admin_film_new")
+     * @param Request $request
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function formAction(){
-        return $this->render('AdminBundle:Film:form.html.twig');
+    public function formAction(Request $request, $id = null){
+
+        $movie = new Movie();
+
+        if ($id != null) {
+            $repo = $this->getDoctrine()->getRepository('ModelBundle:Movie');
+
+            $movie = $repo->findOneById($id);
+        }
+
+        $form = $this->createForm(
+            MovieType::class,
+            $movie,
+            [
+                "method" => "post"
+            ]
+        );
+
+        $form->handleRequest($request);
+
+        // Only persist if form is submitted and validation tests are all ok
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                // Entity persistence
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($movie);
+                $em->flush();
+
+                // Add flash message
+                $this->addFlash('info', 'Votre film a bien été ajouté');
+
+                // Redirection to /new-tag route
+                return $this->redirectToRoute('admin_film_home');
+            } catch (UniqueConstraintViolationException $ex) {
+                // Add flash message
+                $this->addFlash('danger', 'Un tag portant ce nom existe déjà');
+            }
+        }
+
+        return $this->render('AdminBundle:Film:form.html.twig', [
+            "movieForm" => $form->createView()
+        ]);
     }
 
     /**
--- src/ModelBundle/Form/MovieType.php
+++ src/ModelBundle/Form/MovieType.php
@@ -2,7 +2,12 @@
 
 namespace ModelBundle\Form;
 
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +18,76 @@ class MovieType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('title')->add('duration')->add('releaseDate')->add('language')->add('nationality')->add('category')->add('director')->add('actors');
+        $builder
+            ->add(
+                'title',
+                TextType::class,
+                [
+                    "label" => "Titre"
+                ])
+            ->add(
+                'duration',
+                NumberType::class,
+                [
+                    "label" => "Durée"
+                ])
+            ->add(
+                'releaseDate',
+                DateType::class,
+                [
+                    "label" => "Date de sortie"
+                ])
+            ->add(
+                'language',
+                EntityType::class,
+                [
+                    "class" => "ModelBundle\Entity\Language",
+                    "choice_label" => "language",
+                    "placeholder" => "Choisissez une langue",
+                    "label" => "Langue"
+                ])
+            ->add(
+                'nationality',
+                EntityType::class,
+                [
+                    "class" => "ModelBundle\Entity\Nationality",
+                    "choice_label" => "nationality",
+                    "placeholder" => "Choisissez une nationalité",
+                    "label" => "Nationalité"
+                ])
+            ->add(
+                'category',
+                EntityType::class,
+                [
+                    "class" => "ModelBundle\Entity\MovieCategory",
+                    "choice_label" => "category",
+                    "placeholder" => "Choisissez une catégorie",
+                    "label" => "Catégorie"
+                ])
+            ->add(
+                'director',
+                EntityType::class,
+                [
+                    "class" => "ModelBundle\Entity\Artist",
+                    "choice_label" => "name",
+                    "placeholder" => "Choisissez un réalisateur",
+                    "label" => "Réalisateur"
+                ])
+            ->add(
+                'actors',
+                EntityType::class,
+                [
+                    "class" => "ModelBundle\Entity\Artist",
+                    "label" => "Acteur",
+                    "choice_label" => "name",
+                    "multiple" => true
+                ])
+            ->add(
+                'submit',
+                SubmitType::class,
+                [
+                    'label' => 'Valider'
+                ]);
     }
     
     /**
